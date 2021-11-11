422349bb1711d4e1e776916ec5ba1b78300de9bb
usama <mahmoodify@gmail.com>
2021-11-11 16:11:17 +0500

    Parameters processing done

---
+++ _internal/args.py
@@ -0,0 +1,61 @@
+from sys   import argv
+from os    import curdir
+
+######
+# Command-line arguments:
+#          1 => directory-path
+#          2 => recursive-behavior [True/False]
+#          3 => recursion-depth    [0 for infinite]
+####
+class ParseArguments:
+    def __init__(self):
+        self.scriptName = ""
+        self.directoryPath = curdir
+        self.isRecursive = True
+        self.recursionDepth = 0
+        
+        self.parseArguments()
+
+    def parseArguments(self):
+        # Script name
+        if(argv):
+            self.scriptName = argv.pop(0)
+
+        # Directory path
+        if(argv):
+            try:
+                self.directoryPath = argv.pop(0)
+            except:
+                self.directoryPath = curdir
+        
+        # Recursiveness enabled
+        if(argv):
+            value = argv.pop(0).lstrip().rstrip().lower()
+
+            if(value == "false"):
+                self.isRecursive = False
+            else:
+                self.isRecursive = True
+
+        # Recursion depth
+        if(argv):
+            try:
+                self.recursionDepth = int(argv.pop(0))
+            except:
+                self.recursionDepth = 0
+
+        # Spoiling remaining elements
+        while(argv):
+            argv.pop()
+        
+
+    def print(self):
+        rd = self.recursionDepth
+        if(rd == 0):
+            rd = "Infinite"
+        
+        print(f"Script Name:       {self.scriptName}")
+        print(f"Directory Path:    {self.directoryPath}")
+        print(f"Recursion Enabled: {self.isRecursive}")
+        print(f"Recursion Depth:   {rd}")
+        return
---
+++ main.py
@@ -0,0 +1,7 @@
+from _internal.web         import  *
+from _internal.recursion   import  *
+from _internal.args        import  *
+
+arguments = ParseArguments()
+arguments.print()
+
