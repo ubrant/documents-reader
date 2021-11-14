705299a10d28a218d5b96d741597af9765882f63
usama <mahmoodify@gmail.com>
2021-11-15 00:22:26 +0500

    Removing BOM, and adding tuple definition

--- _internal/parsing_helpers.py
+++ _internal/parsing_helpers.py
@@ -1,11 +1,12 @@
-from re import sub
+from typing           import Tuple
+from re               import sub
 
 def getFolderFromFileName(filename: str) -> str:
     tmp = sub("/[^/]*$", "", filename)
     return sub("\\\\[^\\\\]*$", "", tmp)
 
-def readFileWithLineNumbers(filename: str) -> (int, str):
-    f = open(filename)
+def readFileWithLineNumbers(filename: str) -> Tuple[int, str]:
+    f = open(filename, "rU", 4096, "utf-8-sig")
     ln = 0
     for line in f:
         ln += 1
