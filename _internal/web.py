5120a8191f269cb6b9ea188e18a673a2fe0b2c94
usama <mahmoodify@gmail.com>
2021-11-16 20:42:16 +0500

    Creating directory

--- _internal/web.py
+++ _internal/web.py
@@ -1,15 +1,34 @@
+from genericpath import isdir
 from _internal.settings       import Settings
 from _internal.data_elements  import *
 
+from os                       import mkdir, execl, system
+from os.path                  import isdir, exists, join
+
+def ensureDir(dirname: str) -> bool:
+    if exists(dirname):
+        if isdir(dirname):
+            return True
+        else:
+            print(f"Error: {dirname} exists but it is not a directory")
+    else:
+        mkdir(dirname)
+        if isdir(dirname):
+            return True
+    return False
 
 class WebContentGenerator:
     def __init__(self, settings: Settings, data: Data) -> None:
-        self.settings = settings
-        self.data = data
+        self.settings: Settings = settings
+        self.data: Data = data
+        self.written: bool = False
         return
 
     def generateOutput(self) -> None:
         self.printHierarchy()
+        self.written = False
+        if ensureDir(self.settings.outputDir):
+            pass
         return
 
     def printHierarchy(self) -> None:
@@ -24,4 +43,6 @@ class WebContentGenerator:
                         print(f"          > Page({page.id}): {page.title}")
 
     def openOutput(self) -> None:
+        if (self.written):
+            pass
         return
