c8d5e4c2daf4376cb0ffed95138da50e1bacf702
usama <mahmoodify@gmail.com>
2021-11-16 12:33:08 +0500

    Finalizing time stamps

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from datetime              import  datetime
+from datetime              import  datetime, timedelta
+
 from _internal.settings    import  Settings
 from _internal.recursion   import  getDesiredFiles
 from _internal.parser      import  Parser
 from _internal.web         import  WebContentGenerator
 
-# Time-stamp
-startTime = datetime.now()
+
+######
+# Loading Settings
+####
+
+# Time-stamp - Start
+tsStart = datetime.now()
 
 # Global Settings
 settings = Settings()
@@ -13,6 +19,14 @@ settings.print(
     headingLine = "Settings:",
     indent = " > ",
     appendBlankLines = 1)
+
+
+######
+# Loading Files
+####
+
+# Time-stamp - Start Processing Files
+tsStartProcessingFiles = datetime.now()
 
 # Processing Files
 print("Processing Files")
@@ -23,23 +37,50 @@ for file in getDesiredFiles(settings, ".ubd"):
     print(f"Reading {file}")
     parser.loadFile(file)
 
+
+######
+# Writing Files
+####
+
+# Time-stamp - Start Writing Web Content
+tsStartWritingWebContent = datetime.now()
+
 # Generating Web Content
 webGenerator = WebContentGenerator(settings, parser.getParsedData())
 webGenerator.generateOutput()
 webGenerator.openOutput()
 
-# Time Elapsed
-et = (datetime.now() - startTime).total_seconds()
+
+######
+# End
+####
+
+# Time-stamp - End
+tsEnd = datetime.now()
+
+def printElapsedTime(indent: str, parameter: str, dt: timedelta):
+    messageFormat = "{}{} = {:7.3f} {}"
+
+    value = dt.total_seconds()
+    unit = "sec"
+    
+    if (value < 1.0):
+        value = value * 1000
+        unit = "msec"
+    
+    print(messageFormat.format(indent, parameter, value, unit))
 
 print("")
 print(".")
 print("...")
-print(".....")
 print("Completed")
 
-if (et < 1.0):
-    print(f"Total Run-time = {et*1000:0.2f}ms")
-else:
-    print(f"Total Run-time = {et:0.2f}sec")
+print("")
+print("Times taken:")
+printElapsedTime("  > ", "Loading Settings", tsStartProcessingFiles - tsStart)
+printElapsedTime("  > ", "Loading Files   ", tsStartWritingWebContent - tsStartProcessingFiles)
+printElapsedTime("  > ", "Writing Files   ", tsEnd - tsStartWritingWebContent)
+print("")
+printElapsedTime("  > ", "Total", tsEnd - tsStart)
 
 print("")
