2c354c7b9540abd5677380036c75dfd24d369f55
usama <mahmoodify@gmail.com>
2021-11-20 01:36:22 +0500

    Update run-reader.sh

--- run-reader.sh
+++ run-reader.sh
@@ -5,7 +5,7 @@ SCAN_DIR="$PWD/UBD-Docs"
 GENF_DIR="$PWD/Generated-Site"
 
 env --chdir="./Reader"              \
-    python  "main.py"               \
+    python3 "main.py"               \
             "$SCAN_DIR"             \
             "true"                  \
             "1000000"               \
