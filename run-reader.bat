215314267142730ceba05def755cf720dfebb699
usama <mahmoodify@gmail.com>
2021-11-20 02:20:21 +0500

    Changing to include abs path ... correcting images path problem on win

--- run-reader.bat
+++ run-reader.bat
@@ -1,6 +1,12 @@
+echo off
+
 TITLE UB Documentsreader
 
+set cwd=%cd%
+set docs_dir=%cwd%\UBD-Docs
+set site_dir=%cwd%\Generated-Site
+
 CD "Reader"
-python "main.py"  "..\UBD-Docs" "true" "1000000" "..\Generated-Site"
+python "main.py"  %docs_dir% "true" "1000000" %site_dir%
 
 PAUSE
