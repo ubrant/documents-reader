0d58a3d9174e70737187d2d673633b122192eab7
usama <mahmoodify@gmail.com>
2021-11-19 13:54:13 +0500

    Finalizing outline and preliminary script

---
+++ run-reader.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+PWD=$(pwd)
+SCAN_DIR="$PWD/UBD-Docs"
+GENF_DIR="$PWD/Generated-Site"
+
+env --chdir="./Reader"              \
+    python  "main.py"               \
+            "$SCAN_DIR"             \
+            "true"                  \
+            "1000000"               \
+            "$GENF_DIR"
