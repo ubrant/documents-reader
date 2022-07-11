fa6e7758d18884c6a8ea22a8612a9fd26eec8a7d
usama <mahmoodify@gmail.com>
2022-07-12 00:32:54 +0500

    Update run-reader.bat

--- run-reader.bat
+++ run-reader.bat
@@ -3,13 +3,13 @@
 TITLE UBD Reader
 
 set cwd=%cd%
-set do_recursion=true
+set recursion_enabled=true
 set recursion_depth=1000000
 set docs_in_dir=%cwd%\UBD-Docs
 set site_out_dir=%cwd%\Generated-Site
 set site_base_url=https://ubrant.com/
 
 CD "Reader"
-python "main.py"  %docs_in_dir%  %do_recursion%  %recursion_depth%  %site_out_dir%  %site_base_url%
+python "main.py"  %docs_in_dir%  %recursion_enabled%  %recursion_depth%  %site_out_dir%  %site_base_url%
 
 PAUSE
